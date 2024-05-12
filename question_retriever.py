e15851ea62f3d8e62cfbf8ea92ba45d8009d2e03
selfeer <selfeers@gmail.com>
2024-05-12 15:19:05 +0400

    update prompt

--- question_retriever.py
+++ question_retriever.py
@@ -16,7 +16,7 @@ from langchain_core.prompts import PromptTemplate
 
 template = """Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use three sentences maximum and keep the answer as concise as possible.
+Use three sentences maximum and keep the answer as concise as possible. When you provide an answer, also provide a code example if possible.
 Always say "thanks for asking!" at the end of the answer.
 
 {context}
