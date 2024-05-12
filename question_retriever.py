0173c318716586d9fe2d58f9089bf4b0be4ea935
selfeer <selfeers@gmail.com>
2024-05-12 16:22:13 +0400

    optimize imports

--- question_retriever.py
+++ question_retriever.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-import os
 import argparse
+import os
 
-from langchain_community.document_loaders import TextLoader, ToMarkdownLoader
 from langchain_chroma import Chroma
+from langchain_community.document_loaders import TextLoader
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_core.prompts import PromptTemplate
-
 
 template = """Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
