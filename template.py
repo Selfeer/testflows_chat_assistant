d33c3684344002eb47122a01a632ae2e51aa7300
selfeer <selfeers@gmail.com>
2024-05-23 15:04:34 +0400

    update prompt

--- template.py
+++ template.py
@@ -1,5 +1,5 @@
 template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, 
-just say that you don't know, don't try to make up an answer. Use three sentences maximum and keep the answer as 
+just say that you don't know, don't try to make up an answer. keep the answer as 
 concise as possible. When you provide an answer always provide a code example.
 
 {context}
